39d62829030c9ea8ec29c4f68adaff50b50d31de
Alkhanet <92195275+alkhanet26@users.noreply.github.com>
2023-03-14 21:32:08 +0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -7,7 +7,7 @@ sleep 2
 wget --no-check-certificate "https://raw.githubusercontent.com/alkhanet26/install-xderm-mt7620/main/badvpn.ipk" -O /tmp/badvpn.ipk && chmod +x /tmp/badvpn.ipk
 wget --no-check-certificate "https://raw.githubusercontent.com/alkhanet26/install-xderm-mt7620/main/corkscrew.ipk" -O /tmp/corkscrew.ipk && chmod +x /tmp/corkscrew.ipk
 wget --no-check-certificate "https://raw.githubusercontent.com/alkhanet26/install-xderm-mt7620/main/sshpass.ipk" -O /tmp/sshpass.ipk && chmod +x /tmp/sshpass.ipk
-wget --no-check-certificate "https://raw.githubusercontent.com/alkhanet26/install-xderm-mt7620/main/trojan" -O /tmp/trojan.ipk && chmod +x /tmp/trojan.ipk
+wget --no-check-certificate "https://raw.githubusercontent.com/alkhanet26/install-xderm-mt7620/main/trojan.ipk" -O /tmp/trojan.ipk && chmod +x /tmp/trojan.ipk
 wget --no-check-certificate "https://raw.githubusercontent.com/alkhanet26/install-xderm-mt7620/main/v2ray-core.ipk" -O /tmp/v2ray-core.ipk && chmod +x /tmp/v2ray-core.ipk
 wget --no-check-certificate "https://raw.githubusercontent.com/alkhanet26/install-xderm-mt7620/main/v2ray-plugin.ipk" -O /tmp/v2ray-plugin.ipk && chmod +x /tmp/v2ray-plugin.ipk
 clear
@@ -26,6 +26,8 @@ wget -O /bin/fixphp "https://raw.githubusercontent.com/helmiau/openwrt-config/ma
 wget -O /etc/config/uhttpd "https://raw.githubusercontent.com/portalssh/openwrt/main/xderm-mini/uhttpd" && /etc/init.d/uhttpd restart
 wget -O /etc/redsocks.conf "https://raw.githubusercontent.com/portalssh/openwrt/main/xderm-mini/redsocks"
 wget -O /usr/bin/gproxy "https://raw.githubusercontent.com/portalssh/openwrt/main/xderm-mini/gproxy" && chmod +x /usr/bin/gproxy
+wget --no-check-certificate "https://raw.githubusercontent.com/alkhanet26/install-xderm-mt7620/main/xderm-gui.htm" -O /usr/lib/lua/luci/view/xderm-gui.htm && chmod +x /usr/lib/lua/luci/view/xderm-gui.htm
+wget --no-check-certificate "https://raw.githubusercontent.com/alkhanet26/install-xderm-mt7620/main/xderm-gui.lua" -O /usr/lib/lua/luci/controller/xderm-gui.lua && chmod +x /usr/lib/lua/luci/controller/xderm-gui.lua
 clear
 echo -e "All process was completed"
 sleep 1
